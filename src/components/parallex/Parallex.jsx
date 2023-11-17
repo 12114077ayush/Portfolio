081892edc3afa6851ea48a2144df99bd761ba2c3
Ayush choudhary <2003ayushchoudhary@gmail.com>
2023-11-17 09:49:32 +0530

    stars and mountains img format changes

--- src/components/parallex/Parallex.jsx
+++ src/components/parallex/Parallex.jsx
@@ -10,9 +10,9 @@ const Parallex = ({ type }) => {
                 opacity: 0,
                 scale: 0.5
             }} whileInView={{ opacity: 1, scale: 1.5, }} transition={{ duration: 0.75 }}> {type === "My skills" ? "My skills" : "My projects"} </motion.h1>
-            <div className="mountains"></div>
+            <div className="mountains" style={{ backgroundImage: "url(./mountains.png)" }}></div>
             <div className="planet" style={{ backgroundImage: type === "My skills" ? "url(./planets.png)" : "url(./sun.png)" }}></div>
-            <motion.div className="stars" initial={{ opacity: 0, x: -500 }} whileInView={{ opacity: 1, x: 0, }} transition={{ duration: 1 }}></motion.div>
+            <motion.div className="stars" initial={{ opacity: 0, x: -500 }} whileInView={{ opacity: 1, x: 0, }} transition={{ duration: 1 }} style={{ backgroundImage: "url(./stars.png)" }}></motion.div>
         </div>
     );
 }
